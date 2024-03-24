3836983a50d63162cbf97895304e194d7f19aabc
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-24 18:17:36 -0400

    Put card in div

--- Website Project/src/App.tsx
+++ Website Project/src/App.tsx
@@ -3,10 +3,11 @@ import Card from "../components/Card";
 import "./App.css";
 
 function App() {
-  return;
-  <div>
-    <Card></Card>;
-  </div>;
+  return (
+    <div>
+      <Card></Card>
+    </div>
+  );
 }
 
 export default App;
