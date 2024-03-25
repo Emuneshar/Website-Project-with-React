2ad968feac114e1c77b48635ca89817785882e8e
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-25 18:02:31 -0400

    Doing stuff

--- Website Project/src/App.tsx
+++ Website Project/src/App.tsx
@@ -1,12 +1,23 @@
 import Card from "../components/Card";
 import Grid from "../components/Grid";
 
+
+
 function App() {
+  const CardData = [
+    {id:1, title: 'Stuff', content:'Thing thing thing', buttontext:'Book Now'},
+    {id:1, title: 'Stuff', content:'Thing thing thing', buttontext:'Book Now'},
+    {id:1, title: 'Stuff', content:'Thing thing thing', buttontext:'Book Now'},
+  ];
+  
+  
   return (
     <div>
-      <Grid></Grid>
-      <Card></Card>
-      <Card></Card>
+      <Grid>
+        {CardData.map((card) => (
+          <Card key = {card.id}</Card>
+        ))}
+      </Grid>
     </div>
   );
 }
