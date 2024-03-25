eea0f105d6079e5d0622a6eaa3a74942b779a191
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-25 17:13:46 -0400

    Added JSX for grid component

--- Website Project/components/Grid.tsx
+++ Website Project/components/Grid.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-
 const Grid = () => {
-    return(
-        
-    );
-}
+  return (
+    <div className="container text-center">
+      <div className="row row-cols-4">
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+      </div>
+    </div>
+  );
+};
 
 export default Grid;
