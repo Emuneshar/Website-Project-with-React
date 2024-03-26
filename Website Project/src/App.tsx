6e588e322fcb81f7c9b575312cec4747278fff58
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-26 17:14:36 -0400

    Added Buttons to grid and fixed syntax issues

--- Website Project/src/App.tsx
+++ Website Project/src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React from "react";
 import Card from "../components/Card";
 import Grid from "../components/Grid";
 
-
-
 function App() {
   const CardData = [
-    {id:1, title: 'Stuff', content:'Thing thing thing', buttontext:'Book Now'},
-    {id:1, title: 'Stuff', content:'Thing thing thing', buttontext:'Book Now'},
-    {id:1, title: 'Stuff', content:'Thing thing thing', buttontext:'Book Now'},
+    {
+      id: 1,
+      title: "Stuff",
+      content: "Thing thing thing",
+      buttontext: "Book Now",
+    },
+    {
+      id: 1,
+      title: "Stuff",
+      content: "Thing thing thing",
+      buttontext: "Book Now",
+    },
+    {
+      id: 1,
+      title: "Stuff",
+      content: "Thing thing thing",
+      buttontext: "Book Now",
+    },
   ];
-  
-  
+
   return (
-    <div>
-      <Grid>
-        {CardData.map((card) => (
-          <Card key = {card.id}</Card>
-        ))}
-      </Grid>
-    </div>
+    <Grid>
+      <Card
+        title="Card 1"
+        content="Card 1 content"
+        buttonText="Click Me"
+        onButtonClick={() => console.log("Button Clicked!")}
+      />
+      <Card
+        title="Card 1"
+        content="Card 1 content"
+        buttonText="Click Me"
+        onButtonClick={() => console.log("Button Clicked!")}
+      />
+      <Card
+        title="Card 1"
+        content="Card 1 content"
+        buttonText="Click Me"
+        onButtonClick={() => console.log("Button Clicked!")}
+      />
+    </Grid>
   );
 }
 
