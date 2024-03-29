4bbcddb54ae5735bcadb78bd93cab7b35fed35e0
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-29 16:28:39 -0400

    removed some text from the navbar

--- Website Project/components/NavBar.tsx
+++ Website Project/components/NavBar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
             <a className="nav-link" href="/">
-              Home <span className="sr-only">(current)</span>
+              Home <span className="sr-only"></span>
             </a>
           </li>
           <li className="nav-item">
