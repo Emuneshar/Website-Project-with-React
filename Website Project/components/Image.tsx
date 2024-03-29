87589b70849910d0eddf0787460b86c50d06be7b
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-29 18:11:50 -0400

    Fixed Export Statement

--- Website Project/components/Image.tsx
+++ Website Project/components/Image.tsx
@@ -6,4 +6,17 @@ const ImageComponent: React.FC = () => {
   const handleResize = () => {
     setWidth(300);
   };
+
+  return (
+    <div>
+      <img
+        src="../Images/temp_image_one.jpg"
+        alt="Your Image"
+        style={{ width: "${width}px", height: "auto" }}
+      />
+      <button onClick={handleResize}>Resize</button>
+    </div>
+  );
 };
+
+export default ImageComponent;
