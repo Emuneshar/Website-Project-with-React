fc3fa1e8457655a9735f914b8199a83659a11bab
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-29 20:46:34 -0400

    added image 2 and 3 to carousel

--- Website Project/components/Carousel.tsx
+++ Website Project/components/Carousel.tsx
@@ -33,7 +33,7 @@ const Carousel = () => {
       </div>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src="..." className="d-block w-100" alt="..." />
+          <img src="../Images/image3.jpg" className="d-block w-100" alt="..." />
           <div className="carousel-caption d-none d-md-block">
             <h5>First slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
@@ -47,7 +47,7 @@ const Carousel = () => {
           </div>
         </div>
         <div className="carousel-item">
-          <img src="..." className="d-block w-100" alt="..." />
+          <img src="../Images/image2.jpg" className="d-block w-100" alt="..." />
           <div className="carousel-caption d-none d-md-block">
             <h5>Third slide label</h5>
             <p>Some representative placeholder content for the third slide.</p>
