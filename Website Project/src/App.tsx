e1decf8064da5bdec268dc1129e643124209a44d
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-30 22:13:00 -0400

    Adding spacing between carousel and grid

--- Website Project/src/App.tsx
+++ Website Project/src/App.tsx
@@ -21,29 +21,31 @@ function HomePage() {
   return (
     <>
       <Carousel></Carousel>
-      <Grid>
-        <Card
-          title="Package 1"
-          src="../Images/image1.jpg"
-          content="Package 1 info"
-          buttonText="Click Me"
-          onButtonClick={() => console.log("Button Clicked!")}
-        />
-        <Card
-          title="Package 2"
-          src="../Images/image2.jpg"
-          content="Package 2 info"
-          buttonText="Click Me"
-          onButtonClick={() => console.log("Button Clicked!")}
-        />
-        <Card
-          title="Package 3"
-          src="../Images/image3.jpg"
-          content="Package 3 info"
-          buttonText="Click Me"
-          onButtonClick={() => console.log("Button Clicked!")}
-        />
-      </Grid>
+      <div style={{ margin: "20px 0" }}>
+        <Grid>
+          <Card
+            title="Package 1"
+            src="../Images/image1.jpg"
+            content="Package 1 info"
+            buttonText="Click Me"
+            onButtonClick={() => console.log("Button Clicked!")}
+          />
+          <Card
+            title="Package 2"
+            src="../Images/image2.jpg"
+            content="Package 2 info"
+            buttonText="Click Me"
+            onButtonClick={() => console.log("Button Clicked!")}
+          />
+          <Card
+            title="Package 3"
+            src="../Images/image3.jpg"
+            content="Package 3 info"
+            buttonText="Click Me"
+            onButtonClick={() => console.log("Button Clicked!")}
+          />
+        </Grid>
+      </div>
     </>
   );
 }
