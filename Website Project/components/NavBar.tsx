9a894c44c73b99855ef05906878b026674315561
Eshwar Muneshar <Emuneshar@gmail.com>
2024-03-30 22:41:14 -0400

    attempting to fix Navbar responsiveness

--- Website Project/components/NavBar.tsx
+++ Website Project/components/NavBar.tsx
@@ -10,48 +10,57 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-white bg-light border-bottom-gray">
-      <Link className="navbar-brand" to="/">
-        Ultrasound Clinic
-      </Link>
-      <button className="navbar-toggler" type="button" onClick={toggleDropdown}>
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div
-        className={`collapse navbar-collapse ${isDropDownOpen ? "show" : ""}`}
-      >
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">
-              Home <span className="sr-only"></span>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">
-              About
-            </Link>
-          </li>
-          <li className={`nav-item dropdown ${isDropDownOpen ? "show" : ""}`}>
-            <span className="nav-link dropdown-toggle" onClick={toggleDropdown}>
-              Services
-            </span>
-            <div className={`dropdown-menu ${isDropDownOpen ? "show" : ""}`}>
-              <Link className="dropdown-item" to="/services/design">
-                Design
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Ultrasound Clinic
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleDropdown}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`collapse navbar-collapse ${isDropDownOpen ? "show" : ""}`}
+        >
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home <span className="sr-only"></span>
               </Link>
-              <Link className="dropdown-item" to="/services/development">
-                Development
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
               </Link>
-              <Link className="dropdown-item" to="/services/marketing">
-                Marketing
+            </li>
+            <li className={`nav-item dropdown ${isDropDownOpen ? "show" : ""}`}>
+              <span
+                className="nav-link dropdown-toggle"
+                onClick={toggleDropdown}
+              >
+                Services
+              </span>
+              <div className={`dropdown-menu ${isDropDownOpen ? "show" : ""}`}>
+                <Link className="dropdown-item" to="/services/design">
+                  Design
+                </Link>
+                <Link className="dropdown-item" to="/services/development">
+                  Development
+                </Link>
+                <Link className="dropdown-item" to="/services/marketing">
+                  Marketing
+                </Link>
+              </div>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact
               </Link>
-            </div>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">
-              Contact
-            </Link>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
