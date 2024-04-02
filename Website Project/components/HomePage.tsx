8384bbdb2f0a5278f5fa8149b59cce56026d7e60
Eshwar Muneshar <Emuneshar@gmail.com>
2024-04-02 16:31:11 -0400

    Moved HomePage code to separate file

--- Website Project/components/HomePage.tsx
+++ Website Project/components/HomePage.tsx
@@ -1,2 +1,41 @@
 import React from "react";
 import Carousel from "../components/Carousel";
+import Grid from "../components/Grid";
+import Card from "../components/Card";
+
+function HomePage() {
+  return (
+    <>
+      <div style={{ margin: "20px 0" }}>
+        <Carousel></Carousel>
+      </div>
+      <div style={{ margin: "20px 0" }}>
+        <Grid>
+          <Card
+            title="Package 1"
+            src="../Images/image1.jpg"
+            content="Package 1 info"
+            buttonText="Click Me"
+            onButtonClick={() => console.log("Button Clicked!")}
+          />
+          <Card
+            title="Package 2"
+            src="../Images/image2.jpg"
+            content="Package 2 info"
+            buttonText="Click Me"
+            onButtonClick={() => console.log("Button Clicked!")}
+          />
+          <Card
+            title="Package 3"
+            src="../Images/image3.jpg"
+            content="Package 3 info"
+            buttonText="Click Me"
+            onButtonClick={() => console.log("Button Clicked!")}
+          />
+        </Grid>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
--- Website Project/src/App.tsx
+++ Website Project/src/App.tsx
@@ -18,39 +18,4 @@ function App() {
   );
 }
 
-function HomePage() {
-  return (
-    <>
-      <div style={{ margin: "20px 0" }}>
-        <Carousel></Carousel>
-      </div>
-      <div style={{ margin: "20px 0" }}>
-        <Grid>
-          <Card
-            title="Package 1"
-            src="../Images/image1.jpg"
-            content="Package 1 info"
-            buttonText="Click Me"
-            onButtonClick={() => console.log("Button Clicked!")}
-          />
-          <Card
-            title="Package 2"
-            src="../Images/image2.jpg"
-            content="Package 2 info"
-            buttonText="Click Me"
-            onButtonClick={() => console.log("Button Clicked!")}
-          />
-          <Card
-            title="Package 3"
-            src="../Images/image3.jpg"
-            content="Package 3 info"
-            buttonText="Click Me"
-            onButtonClick={() => console.log("Button Clicked!")}
-          />
-        </Grid>
-      </div>
-    </>
-  );
-}
-
 export default App;
