569e46433a3114e53f684bd1299a299c44fefb8f
Eshwar Muneshar <Emuneshar@gmail.com>
2024-04-02 16:48:46 -0400

    Cleaned up import

--- Website Project/src/App.tsx
+++ Website Project/src/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Card from "../components/Card";
-import Grid from "../components/Grid";
 import NavBar from "../components/NavBar";
 import HomePage from "../components/HomePage";
-import Carousel from "../components/Carousel";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../css/App.css";
 
