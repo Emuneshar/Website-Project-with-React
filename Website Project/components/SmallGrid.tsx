75d362eeebefd4f65f7f8b0ae3a11ec323787d2a
Eshwar Muneshar <Emuneshar@gmail.com>
2024-04-07 18:31:16 -0400

    Attempting to fix bug where text doesn't show up

--- Website Project/components/SmallGrid.tsx
+++ Website Project/components/SmallGrid.tsx
@@ -4,10 +4,10 @@ function SmallGrid() {
   return (
     <div className="row">
       <div className="col-4">
-        <img src="../Images/Image1.jpg" />
+        <img src="../Images/Image1.jpg" alt="Image 1" />
       </div>
-      <div className="col-8 text-here" style={{ color: "black !important" }}>
-        Your text here
+      <div className="col-8">
+        <p className="text-dark">Your text here</p>
       </div>
     </div>
   );
