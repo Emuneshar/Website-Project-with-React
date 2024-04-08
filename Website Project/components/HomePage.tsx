5482eeb067a9edb5c682daa8bb8dc3259dc8306e
Eshwar Muneshar <Emuneshar@gmail.com>
2024-04-08 18:15:35 -0400

    Fixed issue where color didn't apply proper;y

--- Website Project/components/HomePage.tsx
+++ Website Project/components/HomePage.tsx
@@ -5,40 +5,43 @@ import Grid from "../components/Grid";
 import Card from "../components/Card";
 import "../css/HomePage.css";
 
-const HomePage: React.FC = () => {
+function HomePage() {
   return (
     <>
-      <SmallGrid />
-      <div style={{ margin: "20px 0" }}>
-        <Carousel></Carousel>
-      </div>
-      <div style={{ margin: "20px 0" }}>
-        <Grid>
-          <Card
-            title="Package 1"
-            src="../Images/image1.jpg"
-            content="Package 1 info"
-            buttonText="Click Me"
-            onButtonClick={() => console.log("Button Clicked!")}
-          />
-          <Card
-            title="Package 2"
-            src="../Images/image2.jpg"
-            content="Package 2 info"
-            buttonText="Click Me"
-            onButtonClick={() => console.log("Button Clicked!")}
-          />
-          <Card
-            title="Package 3"
-            src="../Images/image3.jpg"
-            content="Package 3 info"
-            buttonText="Click Me"
-            onButtonClick={() => console.log("Button Clicked!")}
-          />
-        </Grid>
+      <div className="homepage-container">
+        <SmallGrid />
+        <div style={{ margin: "20px 0" }}>
+          <Carousel></Carousel>
+        </div>
+        <SmallGrid />
+        <div style={{ margin: "20px 0" }}>
+          <Grid>
+            <Card
+              title="Package 1"
+              src="../Images/image1.jpg"
+              content="Package 1 info"
+              buttonText="Click Me"
+              onButtonClick={() => console.log("Button Clicked!")}
+            />
+            <Card
+              title="Package 2"
+              src="../Images/image2.jpg"
+              content="Package 2 info"
+              buttonText="Click Me"
+              onButtonClick={() => console.log("Button Clicked!")}
+            />
+            <Card
+              title="Package 3"
+              src="../Images/image3.jpg"
+              content="Package 3 info"
+              buttonText="Click Me"
+              onButtonClick={() => console.log("Button Clicked!")}
+            />
+          </Grid>
+        </div>
       </div>
     </>
   );
-};
+}
 
 export default HomePage;
