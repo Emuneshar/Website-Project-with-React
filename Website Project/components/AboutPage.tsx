ca33dc0afa7d887108e32451e50eb3d46c69b4fd
Eshwar Muneshar <Emuneshar@gmail.com>
2024-05-02 18:13:33 -0400

    removed old component

--- Website Project/components/AboutPage.tsx
+++ Website Project/components/AboutPage.tsx
@@ -3,12 +3,6 @@ import HorizontalCard from "../components/HorizontalCard";
 import "../css/AboutPage.css";
 
 function AboutPage() {
-  return (
-    <>
-      <div className="card mb-3">
-        <HorizontalCard></HorizontalCard>
-      </div>
-    </>
-  );
+  return <></>;
 }
 export default AboutPage;
