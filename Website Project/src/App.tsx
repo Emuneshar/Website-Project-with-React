b10a23ef9936b465d22a98b8af809f8c38b9aeff
Eshwar Muneshar <Emuneshar@gmail.com>
2024-05-05 18:10:03 -0400

    Added fixed bottom nabar to app

--- Website Project/src/App.tsx
+++ Website Project/src/App.tsx
@@ -4,7 +4,7 @@ import NavBar from "../components/NavBar";
 import HomePage from "../components/HomePage";
 import AboutPage from "../components/AboutPage";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import BottomNavBar from "../components/BottomNavBar";
+import FixedBottomNavBar from "../components/FixedBottomNavbar";
 import "../css/App.css";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
-      <BottomNavBar />
+      <FixedBottomNavBar />
     </Router>
   );
 }
