5b84035e4caed8c8c683f74348b659d90f6a6967
Eshwar Muneshar <Emuneshar@gmail.com>
2024-05-05 18:49:07 -0400

    testing bottom navbar

--- Website Project/components/FixedBottomNavbar.tsx
+++ Website Project/components/FixedBottomNavbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FixedBottomNavbar: React.FC = () => {
   return (
-    <nav className="navbar fixed-bottom bg-body-tertiary">
+    <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Fixed bottom
