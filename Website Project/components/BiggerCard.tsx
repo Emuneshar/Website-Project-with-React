7c171c09b05819f215551e10459314fe85dfd55c
Eshwar Muneshar <Emuneshar@gmail.com>
2024-05-16 13:11:34 -0400

    Grammar

--- Website Project/components/BiggerCard.tsx
+++ Website Project/components/BiggerCard.tsx
@@ -9,7 +9,7 @@ const BiggerCard: React.FC = () => {
         </h5>
         <h6 className="card-subtitle mb-2 text-body-secondary"></h6>
         <p className="card-text">
-          Here at baby sono views we provide exceptional imaging services, for
+          Here at Baby Sono Views we provide exceptional imaging services, for
           you and your family to enjoy. We offer a variety of packages such as
           gender reveals, pregnancy viability and 2D,3D/4D imaging.
         </p>
