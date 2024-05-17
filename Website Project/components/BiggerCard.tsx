ea959d6267a0842c6b1239c354a5da5206c275f3
Eshwar Muneshar <Emuneshar@gmail.com>
2024-05-16 20:51:09 -0400

    Changed card title size

--- Website Project/components/BiggerCard.tsx
+++ Website Project/components/BiggerCard.tsx
@@ -5,9 +5,9 @@ const BiggerCard: React.FC = () => {
   return (
     <div className="card bg-white border-0" style={{ width: "18rem;" }}>
       <div className="card-body">
-        <h2 className="card-title">
+        <h1 className="card-title">
           Providing a once in a lifetime experience for you and your baby
-        </h2>
+        </h1>
         <h6 className="card-subtitle mb-2 text-body-secondary"></h6>
         <p className="card-text">
           Here at Baby Sono Views we provide exceptional imaging services, for
