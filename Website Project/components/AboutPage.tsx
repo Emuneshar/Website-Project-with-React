dacb8af87a455a49773b30dabad459135f130501
Ethan Ramos <ethanramos@ethans-mbp-8.dhcp.hofstra.edu>
2024-05-17 13:07:36 -0400

    Added text and heading to about page

--- Website Project/components/AboutPage.tsx
+++ Website Project/components/AboutPage.tsx
@@ -3,6 +3,21 @@ import HorizontalCard from "../components/HorizontalCard";
 import "../css/AboutPage.css";
 
 function AboutPage() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1 className="text-center">About Us</h1>
+        <p>
+          My name is Neliza Perez, my passion for women’s healthcare led me to
+          become the registered diagnostic medical sonographer that I am today.
+          As I progressed through my career my love for OBGYN grew immensely!
+          Which is why I decided to open my own business BABY SONO VIEWS where I
+          provide exceptional imaging for you and your family to enjoy. My goal
+          is to provide a once in a lifetime experience for you and your baby.
+          Thank you for allowing me to be part of this journey with you.
+        </p>
+      </div>
+    </>
+  );
 }
 export default AboutPage;
