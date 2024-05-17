71f66d5291422825927bc185d75bb0a4775c43e7
Eshwar Muneshar <Emuneshar@gmail.com>
2024-05-16 20:14:29 -0400

    Placeholder text placed in bottom Navbar

--- Website Project/components/BottomNavbar.tsx
+++ Website Project/components/BottomNavbar.tsx
@@ -3,10 +3,10 @@ import "../css/BottomNavBar.css";
 
 const BottomNavbar: React.FC = () => {
   return (
-    <nav className="navbar fixed-bottom bg-body-tertiary">
+    <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Fixed bottom
+          Test
         </a>
       </div>
     </nav>
