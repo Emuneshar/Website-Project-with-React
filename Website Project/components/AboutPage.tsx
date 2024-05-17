b3ae557e220eb253f1ee60cf7df56debcfc90684
Ethan Ramos <ethanramos@ethans-mbp-8.dhcp.hofstra.edu>
2024-05-17 13:24:16 -0400

    Added styling to about page

--- Website Project/components/AboutPage.tsx
+++ Website Project/components/AboutPage.tsx
@@ -7,11 +7,11 @@ function AboutPage() {
     <>
       <div>
         <h1 className="text-center">About Us</h1>
-        <p>
+        <p id="about">
           My name is Neliza Perez, my passion for women’s healthcare led me to
-          become the registered diagnostic medical sonographer that I am today.
-          As I progressed through my career my love for OBGYN grew immensely!
-          Which is why I decided to open my own business BABY SONO VIEWS where I
+          become the registered diagnostic medical sonographer that I am As I
+          progressed through my career my love for OBGYN grew immensely! Which
+          is why I decided to open my own business BABY SONO VIEWS where I
           provide exceptional imaging for you and your family to enjoy. My goal
           is to provide a once in a lifetime experience for you and your baby.
           Thank you for allowing me to be part of this journey with you.
