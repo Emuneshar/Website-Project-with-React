7313c7204d699468a622fac9af016811c7384baa
Eshwar Muneshar <Emuneshar@gmail.com>
2024-05-16 20:50:24 -0400

    removed color layer behind card

--- Website Project/components/BiggerCard.tsx
+++ Website Project/components/BiggerCard.tsx
@@ -3,19 +3,17 @@ import "../css/BiggerCard.css";
 
 const BiggerCard: React.FC = () => {
   return (
-    <div className="Marshmallow">
-      <div className="card bg-white border-0" style={{ width: "18rem;" }}>
-        <div className="card-body">
-          <h2 className="card-title">
-            Proving a once in a lifetime experience for you and your baby
-          </h2>
-          <h6 className="card-subtitle mb-2 text-body-secondary"></h6>
-          <p className="card-text">
-            Here at Baby Sono Views we provide exceptional imaging services, for
-            you and your family to enjoy. We offer a variety of packages such as
-            gender reveals, pregnancy viability and 2D,3D/4D imaging.
-          </p>
-        </div>
+    <div className="card bg-white border-0" style={{ width: "18rem;" }}>
+      <div className="card-body">
+        <h2 className="card-title">
+          Providing a once in a lifetime experience for you and your baby
+        </h2>
+        <h6 className="card-subtitle mb-2 text-body-secondary"></h6>
+        <p className="card-text">
+          Here at Baby Sono Views we provide exceptional imaging services, for
+          you and your family to enjoy. We offer a variety of packages such as
+          gender reveals, pregnancy viability and 2D,3D/4D imaging.
+        </p>
       </div>
     </div>
   );
