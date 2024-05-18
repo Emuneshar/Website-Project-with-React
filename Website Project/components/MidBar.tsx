b07be39644e0ee025737fef1667ee1f0e75851f4
Eshwar Muneshar <Emuneshar@gmail.com>
2024-05-18 16:19:40 -0400

    Created bar to go in middle of home page

---
+++ Website Project/components/MidBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MidBar: React.FC = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <span className="navbar-text">Live. Love. Scan.</span>
+      </div>
+    </nav>
+  );
+};
+
+export default MidBar;
