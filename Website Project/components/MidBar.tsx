2ffe871d05c9d677937881bdc81ef5dc62fa1fbd
Eshwar Muneshar <Emuneshar@gmail.com>
2024-06-03 14:37:46 -0400

    Centered text on midbar and added code for images

--- Website Project/components/MidBar.tsx
+++ Website Project/components/MidBar.tsx
@@ -4,8 +4,10 @@ import "../css/MidBar.css";
 const MidBar: React.FC = () => {
   return (
     <nav className="navbar bg-body-tertiary" id="Thebar">
-      <div className="container-fluid">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <img src="" alt="Left" className="navbar-icon" />
         <span className="navbar-text white-text">Live. Love. Scan.</span>
+        <img src="" alt="Right" className="navbar-icon" />
       </div>
     </nav>
   );
