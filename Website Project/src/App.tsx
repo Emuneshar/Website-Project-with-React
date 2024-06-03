0e4025ec0a49dc94dce40e9f25e2d478e1e9c375
Eshwar Muneshar <Emuneshar@gmail.com>
2024-06-03 15:40:37 -0400

    Added packages to react router

--- Website Project/src/App.tsx
+++ Website Project/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import HomePage from "../components/HomePage";
 import AboutPage from "../components/AboutPage";
+import Packages from "../components/Packages";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import FixedBottomNavBar from "../components/BottomNavbar";
 import "../css/App.css";
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/services/Packages" element={<Packages />} />
       </Routes>
       <FixedBottomNavBar />
     </Router>
